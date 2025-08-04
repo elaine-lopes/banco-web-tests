bdf7d4909b656ab2fbbb0ee82ab275c51ede19dc
elaine-lopes <elopess.rj@gmail.com>
2025-08-04 20:24:57 -0300

    Adicionando screenshot no teste

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -2,12 +2,15 @@ describe('Login', () => {
   beforeEach(() => {
     // Arrange
     cy.visit('http://localhost:4000')
+    cy.screenshot('apos-visitar-pagina')
   })
   it('Login com dados válidos deve permitir acesso ao sistema', () => {
     // Act
     cy.get('#username').click().type('julio.lima')
     cy.get('#senha').click().type('123456')
+    cy.screenshot('após-preencher-dados-validos')
     cy.contains('button', 'Entrar').click()
+    cy.screenshot('após-clicar-no-botao-entrar')
 
     // Assert
     cy.contains('h4', 'Realizar Transferência').should('be.visible')
